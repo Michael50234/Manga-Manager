a8f5b3925930aca53e9272796f654afdc0880222
Michael50234 <michael.shi7m@gmail.com>
2026-04-12 14:53:17 -0400

    initial commit

---
+++ backend/src/prisma/index.ts
@@ -0,0 +1,11 @@
+// We create and export the Prisma client from this file
+
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
