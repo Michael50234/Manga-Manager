5e060bffcd703143fd0d8a5411f40ecce28196a3
Michael50234 <michael.shi7m@gmail.com>
2026-04-16 23:22:48 -0400

    created a route which lets users make changes to their profile

--- backend/src/routers/accounts.ts
+++ backend/src/routers/accounts.ts
@@ -4,6 +4,7 @@ import { prisma } from '~/prisma';
 import { createJWTToken } from "~/utils";
 import { JWTMiddleware } from "~/middleware/authentication";
 import { UserUpdateArgs } from "generated/prisma/models";
+import { updateSchema } from "~/validators/accounts";
 
 export const accountsRouter = express.Router();
 
@@ -69,22 +70,45 @@ accountsRouter.post("/sign-up", async (req, res) => {
 
 // UserId will be set through JWTMiddleware so we dont need it as a route parameter
 // This route lets users change their nickname, bio, and email
-accountsRouter.patch("/update", JWTMiddleware, (req, res) => {
+accountsRouter.patch("/update", JWTMiddleware, async (req, res) => {
+    // Validate the request body using a zod schema
+    const validated = updateSchema.safeParse(req.body);
     
+    if(!validated.success) {
+        res.status(400).json({
+            detail: `Failed to save changes: ${validated.error.message}`
+        });
+        return;
+    }
+
+    const requestBody = validated.data;
 
     const user = req.user;
 
-    const data = {}
+    const data: UserUpdateArgs['data'] = {};
 
-    if(req.body.nickname) {
-        data.nickanme = req.body.nickname;
+    // Add the fields we want to update to the data attribute
+    if(requestBody.nickname) {
+        data.nickname = requestBody.nickname;
     }
 
-    if(req.body.bio) {
-        data.bio = req.body.bio;
+    if(requestBody.bio) {
+        data.bio = requestBody.bio;
     }
 
-    if(req.body.email) {
-        data.email = req.body.email;
+    if(requestBody.email) {
+        data.email = requestBody.email;
     }
+
+    await prisma.user.update({
+        where: {
+            id: user!.id
+        },
+        data,
+    });
+
+    return res.status(200).json({
+        detail: "Successfully saved changes to user"
+    })
+
 });
--- backend/src/validators/accounts.ts
+++ backend/src/validators/accounts.ts
@@ -1,1 +1,11 @@
 import z from "zod";
+
+export const updateSchema = z.object({
+    bio: z.string().min(1, "Bio must be at least one charecter long").optional(),
+    email: z.string().refine((email) => {
+        return email.includes("@")
+    }, {
+        message: "Please pass a valid email"
+    }).optional(),
+    nickname: z.string().min(1, "Nickname cannot be empty").max(20, "Nickname must be shorter than 20 charecters"),
+})
